64c19643b83b88485cf6f31383e96e2693fcf6ec
ardash054 <ardash054@gmail.com>
2025-02-11 11:58:56 +0300

    header

---
+++ components/shared/categories.tsx
@@ -0,0 +1,21 @@
+import {cn} from '@/lib/utils';
+
+interface Props {
+    className?: string;
+}
+
+const cats = ['Все', 'Мясные', 'Острые', 'Сладкие', 'Вегетарианские', 'С курицей', 'Ещё']
+
+const activIndex = 0;
+
+export const Categories: React.FC<Props> = ({className}) => {
+    return (
+        <div className={cn('border border-b', className)}>
+            {cats.map((cat, index)=>
+            <a className={cn('border border-b', activIndex === index&&'')}
+        key={index}>
+                <button>{cat}</button>
+            </a>)}
+        </div>
+    );
+}
