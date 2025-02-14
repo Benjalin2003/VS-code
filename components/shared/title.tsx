e0190049dfbe24ec625fe05e5f3bf6856ec144f7
ardash054 <ardash054@gmail.com>
2025-02-14 10:38:56 +0300

    categories

---
+++ components/shared/title.tsx
@@ -0,0 +1,21 @@
+import clsx from "clsx";
+import React from "react";
+
+type TitleSize = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
+
+interface Props {
+    size?: TitleSize;
+    className?: string;
+    text: string;
+}
+
+export const Title: React.FC<Props> = ({text, size = 'sm', className }) => {
+    const mapTagBySize = {
+        xs: 'h5',
+        sm: 'h4',
+        md: 'h3',
+        lg: 'h2',
+        xl: 'h1',
+        '2xl': 'h1',
+    }
+}
