6f850d5963d557b4505aa5e0c99db97b7233b48b
ardash054 <ardash054@gmail.com>
2025-02-18 12:00:39 +0300

    container

--- components/shared/categories.tsx
+++ components/shared/categories.tsx
@@ -4,18 +4,25 @@ interface Props {
     className?: string;
 }
 
-const cats = ['Все', 'Мясные', 'Острые', 'Сладкие', 'Вегетарианские', 'С курицей', 'Ещё']
+const cats = ['Все', 'Мясные', 'Острые', 'Сладкие', 'Вегетарианские', 'С курицей', 'Ещё'];
 
-const activIndex = 0;
+const activeIndex = 0;
 
 export const Categories: React.FC<Props> = ({className}) => {
     return (
-        <div className={cn('border border-b', className)}>
-            {cats.map((cat, index)=>
-            <a className={cn('border border-b', activIndex === index&&'')}
-        key={index}>
-                <button>{cat}</button>
-            </a>)}
+        <div className={cn('inline-flex gap-1 bg-gray-50 p-1 rounded-2xl', className)}>
+            {cats.map((cat, index)=> (
+                <a
+                    key={index}
+                    className={cn('flex items-center font-bold h-11 rounded-2xl px-5',
+                        activeIndex === index && 'bg-white shadow-md shadow-gray-200 text-primary'
+                    )}
+                >
+                    <button>
+                        {cat}
+                    </button>
+                </a>
+            ))}
         </div>
     );
-}
+};
--- components/shared/title.tsx
+++ components/shared/title.tsx
@@ -17,5 +17,19 @@ export const Title: React.FC<Props> = ({text, size = 'sm', className }) => {
         lg: 'h2',
         xl: 'h1',
         '2xl': 'h1',
-    }
-}
+    } as const;
+
+    const mapClassNameBySize = {
+        xs: 'text-[16px]',
+        sm: 'text-[22px]',
+        md: 'text-[26px]',
+        lg: 'text-[32px]',
+        xl: 'text-[40px]',
+        '2xl': 'text-[48px]',
+    } as const;
+
+    return React.createElement(
+        mapTagBySize[size],
+        { className: clsx(mapClassNameBySize[size], className) },
+    );
+};
